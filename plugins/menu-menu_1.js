d31a380c8df0bb461cc52a551e8c2c5c5d307c45
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-21 10:28:23 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/menu-menu_1.js
+++ plugins/menu-menu_1.js
@@ -4,7 +4,9 @@ Codigo abierto - Dejar creditos
 Created by https://github.com/BrunoSobrino 
 
 */
-/*import { xpRange } from '../lib/levelling.js'
+
+/*
+import { xpRange } from '../lib/levelling.js'
 const { levelling } = '../lib/levelling.js'
 import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
@@ -388,4 +390,6 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}/*
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+/*
