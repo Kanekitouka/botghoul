45b479eef48c5a9b1a57035809c235e2222ab928
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-06-22 00:36:30 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

---
+++ plugins/downloader-playlist.js
@@ -0,0 +1,44 @@
+/* Created by https://github.com/BrunoSobrino */
+       /* Diseño by Yameko-Bot V1*/
+
+import fs from 'fs'
+import yts from "yt-search"
+let handler = async (m, { text, conn, args, command, usedPrefix }) => {
+let imagen1 = fs.readFileSync('./src/nuevobot.jpg') 
+let imagen2 = fs.readFileSync('./src/Pre Bot Publi.png')
+let search = await yts(args.join(" "))
+let listSerch = []
+let teskd = `𝐌𝐮𝐬𝐢𝐜𝐚 𝐫𝐞𝐥𝐚𝐜𝐢𝐨𝐧𝐚𝐝𝐚 𝐜𝐨𝐧: ${args.join(" ")}`
+const sections = [{
+title: `ⓡⓔⓢⓤⓛⓣⓐⓓⓞⓢ`,
+rows: listSerch }]
+const listMessage = {
+text: teskd,
+footer: '𝐄𝐥𝐢𝐣𝐚 𝐮𝐧𝐚 𝐨𝐩𝐜𝐢𝐨𝐧 𝐲 𝐩𝐫𝐞𝐜𝐢𝐨𝐧𝐞 𝐄𝐧𝐯𝐢𝐚𝐫',
+title: " 『 𝗠𝗨𝗦𝗜𝗖𝗔 𝗥𝗘𝗟𝗔𝗖𝗜𝗢𝗡𝗔𝗗𝗔 』",
+buttonText: "[♦ 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 ♦]",
+sections}
+const fake = { quoted: {
+key : {
+remoteJid: '6283136505591-1614953337@g.us',
+participant : '0@s.whatsapp.net'},
+message: {
+orderMessage: {
+itemCount: 9999999,
+status: 1,
+surface: 1,
+message: '𝑇ℎ𝑒 𝑀𝑦𝑠𝑡𝑖𝑐 - 𝐵𝑜𝑡 - 𝑀𝐷', 
+orderTitle: `Mystic`,
+thumbnail: imagen1, 
+sellerJid: '0@s.whatsapp.net'}}}}
+if (command == 'playlist') {
+for (let i of search.all) {
+listSerch.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp3 ${i.url}`})} 
+conn.sendMessage(m.chat, listMessage, fake)}
+if (command == 'playlist2') {
+for (let i of search.all) {
+listSerch.push({title: i.title, description: `Autor: ${i.author.name} / ${i.timestamp}`, rowId: `${usedPrefix}ytmp4 ${i.url}`})} 
+conn.sendMessage(m.chat, listMessage, fake)} 
+}  
+handler.command = /^playlist|playlist2$/i
+export default handler
